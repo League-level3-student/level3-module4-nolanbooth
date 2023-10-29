9f714b405fe4795695635768b65f96a6cb1cd69b
league <a@b.c>
2023-10-28 18:01:11 -0700

    eeee

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -17,10 +17,12 @@ public class Hangman implements KeyListener {
 	public static JPanel panel;
 	public static JLabel label;
 	public static String text;
-	public static Stack<String>  asdf;
+	public static Stack<String> asdf;
 	public static Random ran;
-	public static String dictionary;
-	
+	public static String theWord;
+	public static boolean playingGame = false;
+	public static boolean canGuess = false;
+	public static String display;
 	public static void main(String[] args) {
 		new _03_Hangman.Hangman().run();
 
@@ -32,8 +34,7 @@ public class Hangman implements KeyListener {
 		label = new JLabel();
 		text = "Press enter to start";
 		asdf = new Stack<String>();
-		
-		
+
 		frame.add(panel);
 		panel.add(label);
 		label.setText(text);
@@ -56,24 +57,69 @@ public class Hangman implements KeyListener {
 		// TODO Auto-generated method stub
 		int keyCode = e.getKeyCode();
 
-		if (keyCode == 10) {
-			String bingus = JOptionPane.showInputDialog(null, "bingus type a number from 0 to 266");
+		if (keyCode == 10 && playingGame == false) {
+			playingGame = true;
+
+			String bingus = JOptionPane.showInputDialog(null,
+					"bingus type a number from 0 to 266, how many times do you want to play");
 			int bingusNumber = Integer.parseInt(bingus);
+
 			if (bingusNumber <= 0 || bingusNumber >= 266) {
 				// end program or something
 				System.exit(0);
 
 			} else {
-				for(int i = 0; i < bingusNumber; i++) {
-				//push words from the dictionary to this stack	asdf.push();
-					//asdf.push(Utilities.readRandomLineFromFile();
+				bill: for (int i = 0; i < bingusNumber; i++) {
+					// push words from the dictionary to this stack asdf.push();
+					String word = Utilities.readRandomLineFromFile("dictionary.txt");
+					if (asdf.contains(word)) {
+						i -= 1;
+					} else {
+						asdf.push(word);
+					}
+					if (asdf.size() == bingusNumber) {
+						break bill;
+					}
+
 				}
-				//make something that detects if a character is matching a character in the chosen word
-				
-				
-				
-				
-				
+				// make something that detects if a character is matching a character in the
+				// chosen word
+				// asdf now has a BingusNumber amount of random words
+				theWord = asdf.pop();
+
+				 display = theWord.replaceAll("[a-zA-z]", "_");
+
+				label.setText("The word is: " + theWord + " How it will look like: " + display);
+
+				JOptionPane.showMessageDialog(null, "Start guessing letters");
+				canGuess = true;
+			}
+
+		} else {
+			if (canGuess == true) {
+				String letter = e.getKeyChar() + "";
+
+				System.out.println(letter);
+
+				if (theWord.contains(letter)) {
+					//all good
+					
+					for (int k = 0; k < theWord.length(); k++) {
+						if(letter == (theWord.charAt(k)+"")) {
+							System.out.println("the problem is not the if statemt");
+							StringBuilder thing = new StringBuilder(display);
+							char character = letter.charAt(0);
+							thing.setCharAt(k, character);
+							display = thing.toString();
+							System.out.println("the word = " + display);
+						}
+					}
+
+				} else {
+					// take away lives
+					System.out.println("Does not contain");
+				}
+
 			}
 
 		}
